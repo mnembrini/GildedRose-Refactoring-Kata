cd288926e3d66185732b89edeb1c8d30d4fff0c6
Marco Nembrini <marco@3ap.ch>
2019-03-14 13:00:31 +0100

    use Update Rules, increase gradle version

--- Java-Spock/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Spock/src/main/java/com/gildedrose/GildedRose.java
@@ -1,22 +1,60 @@
 package com.gildedrose;
 
+import java.util.List;
+
 class GildedRose {
     Item[] items;
 
+    List<UpdateRule> rules;
+
     public GildedRose(Item[] items) {
         this.items = items;
+        UpdateRule defaultRule = new UpdateRule((item -> item.quality - 1));
+        UpdateRule agedBrie = new UpdateRule("Aged Brie", item -> item.quality + 1);
+        UpdateRule backstagePasses = new UpdateRule("Backstage passes", item -> {
+            if (item.sellIn > 10) {
+                return item.quality + 1;
+            }
+            if (item.sellIn > 5) {
+                return item.quality + 2;
+            }
+            if (item.sellIn > 0) {
+                return item.quality + 3;
+            } else {
+                return 0;
+            }
+        });
+        UpdateRule sulfuras = new UpdateRule("Sulfuras", item -> item.quality, item -> item.sellIn);
+        sulfuras.maxQuality = Integer.MAX_VALUE;
+
+        this.rules = List.of(
+                defaultRule,
+                agedBrie,
+                backstagePasses,
+                sulfuras
+
+        );
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
-            updateQuality(items[i]);
+            for (UpdateRule rule : rules) {
+                if (rule.applies(items[i].name)) {
+                    rule.apply(items[i]);
+                }
+            }
 
-            updateSellIn(items[i]);
         }
     }
 
     private void updateQuality(Item item) {
+
+
+        if (item.quality > 50) {
+            item.quality = 50;
+        }
+
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
@@ -60,16 +98,6 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
             }
-        }
-    }
-
-    private void updateSellIn(Item item) {
-        switch (item.name) {
-            case "Sulfuras, Hand of Ragnaros":
-                return;
-            default:
-                item.sellIn--;
-                return;
         }
     }
 
---
+++ Java-Spock/src/main/java/com/gildedrose/UpdateRule.java
@@ -0,0 +1,47 @@
+package com.gildedrose;
+
+import java.util.function.Function;
+
+public class UpdateRule {
+
+    String namePrefix = "";
+    Function<Item, Integer> updateQuality;
+    Function<Item, Integer> updateSellIn = (item) -> item.sellIn - 1;
+    int maxQuality;
+
+    public UpdateRule(Function<Item, Integer> updateQuality) {
+        this.updateQuality = updateQuality;
+    }
+
+    public UpdateRule(String namePrefix, Function<Item, Integer> updateQuality) {
+        this.namePrefix = namePrefix;
+        this.updateQuality = updateQuality;
+    }
+
+    public UpdateRule(String namePrefix, Function<Item, Integer> updateQuality, Function<Item, Integer> updateSellIn) {
+        this.namePrefix = namePrefix;
+        this.updateQuality = updateQuality;
+        this.updateSellIn = updateSellIn;
+    }
+
+    boolean applies(String name) {
+        return name.startsWith(namePrefix);
+    }
+
+    void apply(Item item) {
+        item.quality = updateQuality.apply(item);
+        item.quality = clamp(item.quality, 0, 50);
+
+        item.sellIn = updateSellIn.apply(item);
+    }
+
+    private int clamp(int value, int min, int max) {
+        if (value > max) {
+            return max;
+        }
+        if (value < min) {
+            return min;
+        }
+        return value;
+    }
+}
